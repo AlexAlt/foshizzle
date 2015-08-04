191d31e3c7a1d125c8f5aa9d158c716c38287846
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-03 19:00:44 -0700

    Fixes conditionals and split argument

--- lib/foshizzle_case.rb
+++ lib/foshizzle_case.rb
@@ -1,12 +1,11 @@
 class String
   define_method(:foshizzle) do
-    split_sentence = self.split(" ")
+    split_sentence = self.split("")
     foshizzle_sentence = []
     split_sentence.each do |letter|
       if letter.eql?("s")
         foshizzle_sentence.push("z")
-      elsif letter.eql?("s")
-      elsif
+      else
         foshizzle_sentence.push(letter)
       end
     end
